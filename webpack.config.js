93930f85ae687dc9cf0f159b3a4524c1e7e784b6
Matthew Roseman <mroseman95@gmail.com>
2017-10-05 15:36:53 -0400

    added express react boilerplate
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, './build');
+var APP_DIR = path.resolve(__dirname, './src/client');
+
+const config = {
+    entry: {
+        main: APP_DIR + '/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: BUILD_DIR
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }]
+            },
+            {
+                test: /\.jsx?$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['react', 'es2015']
+                    }
+                }]
+            }
+        ]
+    }
+};
+
+module.exports = config;
